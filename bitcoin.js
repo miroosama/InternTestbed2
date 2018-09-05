c8a5c70d0af99c03b30d495a7b8ac3b0a1241b27
unknown <grayson.cowing@gmail.com>
2018-09-05 12:09:03 -0400

    addresses and keys

--- bitcoin.js
+++ bitcoin.js
@@ -5,13 +5,34 @@ var netwrk = bitcoin.networks.testnet;
 
 var mnemonic = "fun swamp jump history obvious scare struggle deputy cannon village buzz state power play expose moral million lift gravity size chalk grocery scout toss";
 var seed = bip39.mnemonicToSeed(mnemonic);
+var root = bitcoin.bip32.fromSeed(seed)
+var path = "m/44'/1'/0'/0/0"
+var child = root.derivePath(path)
 
-var Tree = bitcoin.HDNode.fromSeedBuffer(seed, netwrk);
+
+var Tree = bitcoin.bip32.fromSeed(seed, netwrk);
 var Leaf = Tree.deriveHardened(44).deriveHardened(1).deriveHardened(0).derive(0).derive(0);
-console.log(Leaf.getAddress());
+var Leaf2 = Tree.deriveHardened(44).deriveHardened(1).deriveHardened(0);
 
-//Next get the private key
-//var Leaf2 = node.deriveHardened(44).deriveHardened(1).deriveHardened(0);
+console.log(getPublicKey(Leaf))
+console.log(getPrivateKey(Leaf))
+console.log(getAddress(Leaf))
 
-//https://coinomi.com/recovery-phrase-tool.html
-//https://iancoleman.io/bip39/
+function getAddress(node){
+    var publicKeyHash = bitcoin.crypto.hash160(node.publicKey);
+    return bitcoin.address.toBase58Check(publicKeyHash, netwrk.pubKeyHash);
+}
+
+function getPublicKey(node){
+    return node.publicKey.hexSlice()
+}
+
+function getPrivateKey(node){
+    return node.toWIF()
+}
+
+
+
+
+// https://coinomi.com/recovery-phrase-tool.html
+// https://iancoleman.io/bip39/
