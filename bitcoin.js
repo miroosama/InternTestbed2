fab3777b60efa3aff9fb21b6ef562425b9d29f5a
miroosama <o.miro650@gmail.com>
2018-09-06 18:08:50 -0400

    cleandynamic1

--- bitcoin.js
+++ bitcoin.js
@@ -1,20 +1,15 @@
 var bip39 = require('bip39');
 var bitcoin = require('bitcoinjs-lib');
 
-<<<<<<< HEAD
 
 var netwrk = bitcoin.networks.testnet;
 
-=======
-var network = bitcoin.networks.testnet; // bitcoin.networks.bitcoin for mainnet
->>>>>>> d43d36a69dd498d67934fcff7d992fc7fb87a49f
 var mnemonic = "fun swamp jump history obvious scare struggle deputy cannon village buzz state power play expose moral million lift gravity size chalk grocery scout toss";
 var seed = bip39.mnemonicToSeed(mnemonic);
 var root = bitcoin.bip32.fromSeed(seed, network);
 // HDNode was deprecated: https://github.com/bitcoinjs/bitcoinjs-lib/issues/1206
 // and https://github.com/bitcoinjs/bitcoinjs-lib/issues/1047
 
-<<<<<<< HEAD
 var Tree = bitcoin.bip32.fromSeed(seed, netwrk);
 var Leaf = Tree.deriveHardened(44).deriveHardened(1).deriveHardened(0).derive(0).derive(0);
 // console.log(Leaf.getAddress());
@@ -48,9 +43,6 @@ const child = root.derivePath(path)
 
 var publicKeyHash = bitcoin.crypto.hash160(child.publicKey);
 var address1 = bitcoin.address.toBase58Check(publicKeyHash, netwrk.pubKeyHash);
-=======
-const node = root.deriveHardened(44).deriveHardened(1).deriveHardened(0).derive(0).derive(0)
->>>>>>> d43d36a69dd498d67934fcff7d992fc7fb87a49f
 
 console.log(getAddress(node, network));
 
