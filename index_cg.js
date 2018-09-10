bd49150becc6004bf55d7fac3de9bf815936fb0c
celestegerard <celeste.gerard@gmail.com>
2018-09-10 14:27:44 -0400

    added signature

--- index_cg.js
+++ index_cg.js
@@ -3,7 +3,6 @@ var bitcoin = require('bitcoinjs-lib');
 const request = require('request');
 
 let seed
-const utxo = [];
 
 
 const Wallet = (() => {
@@ -33,5 +32,63 @@ const Wallet = (() => {
             address = bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address;
         }
         
+    }
+})
+
+const Transaction = (() => {
+
+    API_URL =  'https://testnet.blockexplorer.com/api/addr/'
+    const utxo = [];
+
+    let addr
+    let balance
+    let pubkey
+    let privkey
+    let txid
+    
+    return class {
+        constructor() {
+            this.addr = addr;
+            this.pubkey = pubkey;
+            this.prikey = privkey;
+            this.tx = tx;
+            this.txid = txid;
+            this.balance = balance;
+        }
+
+        deriveAddress(seed) {
+            const network = bitcoin.networks.testnet;
+            const root = bitcoin.bip32.fromSeed(seed, network);
+            const path = "m/44'/1'/0'/0/0";
+            const node = root.deriveHardened(44).deriveHardened(1).deriveHardened(0).derive(0).derive(0)
+            addr = bitcoin.payments.p2pkh({ pubkey: node.publicKey, network }).address
+        }
+
+        getUtxo(addr) {
+            request.get(API_URL + addr + '/utxo', (err, req, body) => {
+                tx = JSON.parse(body)
+                utxo.push(tx)
+                console.log(tx)
+            })
+        }
+
+        getBalance(addr) {
+            request.get(API_URL + addr + '/balance', (err, req, body) => {
+                balance = JSON.parse(body)
+                console.log(balance)
+            })
+        }
+
+        createTransaction() {
+            let transaction = new bitcoin.TransactionBuilder(network);
+            tx.id
+        }
+
+        signTransaction() {
+            privkey = node.toWIF()
+            let signature = bitcoin.ECPair.fromWIF(prikey, network);
+            tx.sign(0, signature)
+        }
+
     }
 })
