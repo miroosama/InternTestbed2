6faa3e1bece9aa0f2e40977a9d9e9b376a9827c7
unknown <grayson.cowing@gmail.com>
2018-09-10 11:25:45 -0400

    readline etc

---
+++ UI.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("say something: ", (answer) =>{
+    switch(answer){
+        case 'hello':
+            saySomething('hey');
+            break;
+        case 'goodbye':
+            saySomething('bye');
+            break;
+        default:
+            saySomething('something')
+    }
+    rl.close();
+})
+
+function saySomething(something){
+    console.log(something)
+}
