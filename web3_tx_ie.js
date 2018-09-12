60729c526bb7879885ee00eab630b89a76360a85
igoreskin <eskinhd@gmail.com>
2018-09-12 14:37:06 -0400

    web3 transaction, bitcoin loop

--- web3_tx_ie.js
+++ web3_tx_ie.js
@@ -11,8 +11,8 @@ const account2 = '0xf901367518aCE01dcE42afA84be17336f84A4f7c';
 // This is a Metamask account: 
 const account3 = '0xfaa3ca002157629651ef65f304f98b46e7dc6ba8';
 
-const privateKey1 = Buffer.from('0x2d57ab0d19019456b496ac10ed9a9cf1ed73aaf4959c472362ef72b3ece435ad');
-const privateKey2 = Buffer.from('0x9c832f83963ac11986ef0f55a3a6e6e73a5eb24d9edff528eab71b1eb91f0542');
+const privateKey1 = Buffer.from('2d57ab0d19019456b496ac10ed9a9cf1ed73aaf4959c472362ef72b3ece435ad', 'hex');
+const privateKey2 = Buffer.from('9c832f83963ac11986ef0f55a3a6e6e73a5eb24d9edff528eab71b1eb91f0542', 'hex');
 
 const privateKey3 = Buffer.from('f78eced94575202245e06771316dfd26ee704df351790d322f156dace479bcfc', 'hex');
 
@@ -31,33 +31,31 @@ web3.eth.getBalance(account3, (err, bal) => {
     web3.utils.fromWei(bal, 'ether'))
 })
 
-web3.eth.getTransactionCount(account1, (err, txCount) => {
+web3.eth.getTransactionCount(account2, (err, txCount) => {
   // Build the transaction: 
   const txObject = {
     nonce: web3.utils.toHex(txCount),
-    to: account2,
-    value: web3.utils.toHex(web3.utils.toWei('1', 'ether')),
+    to: account1,
+    value: web3.utils.toHex(web3.utils.toWei('0.5', 'ether')),
     gasLimit: web3.utils.toHex('21000'),
     gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
   }
 
+  console.log(txObject)
+
   // Sign the transaction: 
   const tx = new Tx(txObject);
-  tx.sign(privateKey3)
+  tx.sign(privateKey2)
 
   const serializedTransaction = tx.serialize();
   const raw = '0x' + serializedTransaction.toString('hex');
 
   // Broadcast the transaction: 
   web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+    console.log(raw)
     console.log('txHash:', txHash)
   })
 })
-
-
-
-
-
 
 
 
