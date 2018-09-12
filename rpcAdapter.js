fc5be1f2cf83665f51c94cc13a7204e3a23ff7fb
unknown <grayson.cowing@gmail.com>
2018-09-12 13:54:54 -0400

    safety commit

--- rpcAdapter.js
+++ rpcAdapter.js
@@ -2,7 +2,8 @@ const Client = require('bitcoin-core');
 const client = new Client({ 
     host: '18.188.236.212', 
     network: 'testnet', 
-    username: 'ubuntu'
+    username: 'testuser',
+    password: 'testpass'
  });
 
 client.getInfo().then((help) => console.log(help));
