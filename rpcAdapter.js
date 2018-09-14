b271af2e808bdf4e8670cef2cdd32387795fb960
unknown <grayson.cowing@gmail.com>
2018-09-14 16:37:48 -0400

    eod commit friday 9/14

--- rpcAdapter.js
+++ rpcAdapter.js
@@ -1,9 +1,44 @@
-const Client = require('bitcoin-core');
+const Client = require('bitcoin-core')
 const client = new Client({ 
-    host: '18.188.236.212', 
+    headers: true,
+    host: '18.222.107.97', 
     network: 'testnet', 
-    username: 'testuser',
-    password: 'testpass'
- });
+    username: 'btcuser',
+    password: 'btcpassword'
+})
+    
+client.getWalletInfo( (promise) => {
+    promise
+    .then( (resp) => console.log(resp) )
+    .catch( (err) => console.log(err) )
+})
+const balance = await new Client({ 
+    headers: true,
+    host: '18.222.107.97', 
+    network: 'testnet', 
+    username: 'btcuser',
+    password: 'btcpassword' 
+}).getBalance({
+    account: '*',
+    minconf: 0
+  });
+    
 
-client.getInfo().then((help) => console.log(help));
+
+
+
+// client.getInfo().then((help) => console.log(help));
+// client.getbalance()
+// console.log('wassssuuuuuup')
+
+//     function(err, body) {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     if(body) { (body) => {
+//         body.then()
+//     }
+
+//     }
+//     console.log('Difficulty: ' + difficulty.then();
+// })
