a04435654f5be43f0b7f6ea3f7e8cf73e6048a77
unknown <grayson.cowing@gmail.com>
2018-09-18 16:29:43 -0400

    refactoring

--- test/rpcAdapter.test.js
+++ test/rpcAdapter.test.js
@@ -5,13 +5,7 @@ test("rpcPost returns an object", () => {
 })
 
 test("rpcPost returns a promise that can be resolved", () => {
-<<<<<<< HEAD
   return RPCAdapter.post('getblockchaininfo').then(resp => {
-=======
-  // console.log(newRPC.rpcPost('getblockchaininfo'))
-  return newRPC.rpcPost('getblockchaininfo').then(resp => {
-    console.log(resp)
->>>>>>> 73b2fbf812b92a961470fb31f611a64bb1bb1189
     expect(Object.keys(resp).length).toBeGreaterThan(0);
   })
 })
