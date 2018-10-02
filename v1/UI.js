3c1c3bc5fa4b480e070a6db5e4552e7796eef87d
celestegerard <celeste.gerard@gmail.com>
2018-10-02 13:17:03 -0400

    required dependencies

--- v1/UI.js
+++ v1/UI.js
@@ -1,8 +1,55 @@
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("http://13.58.39.53:8545"));
-const ethereumjs = require('ethereumjs-tx');
-const readline = require('readline');
 const fs = require('fs');
+const program = require('commander')
 
+const rl = commander.createInterface({ 
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+const User = (() => {
+    return class{
+        constructor() {
+            this.mnemonic = "";
+            this.account = JSON.parse(account)
+        }
+        
+        startSession() {
+            rl.prompt();
+    //         if(this.account !== false){
+    //             rl.question("which coin? (1) Bitcoin, (2) Ethereum", (answer) => {
+    //                 switch(answer){
+    //                 case '1 || Bitcoin || BTC':
+    //                 this.bitcoin();
+    //                 break;
+    //               case '2 || Ethereum || ETH':
+    //                 this.ethereum();
+    //                 break;
+    //             }
+    //         })
+    //     } else {
+    //         rl.question("Sign in with mnemonic: ", (answer) =>{
+    //             switch(answer){
+    //                 default:
+    //                 this.mnemonic = answer
+    //                 this.createEtherWallet(answer)
+    //             }
+    //         })
+    //     }
+    // }
+}
+
+// {btc: {create: create(), import: import()}, ether: eth.create}
+// rl "which coin?"
+// for(loop through coins)
+// answer == btc
+// bitcoin
+
+
+
+})();
+
+let user = new User()
+user.startSession()
 
 
