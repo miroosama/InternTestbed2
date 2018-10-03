84a2949deea97ce59a41bf26a687d053f35993ea
celestegerard <celeste.gerard@gmail.com>
2018-10-03 12:06:03 -0400

    pre-pull

--- classes/BTCWallet.js
+++ classes/BTCWallet.js
@@ -8,18 +8,19 @@ class Wallet {
       this.mnemonic = mnemonic
     }
 
-    createOrUpdateAccount() {
+    createAccount() {
         let seed = bip39.mnemonicToSeed(this.mnemonic);
         let root = bitcoin.bip32.fromSeed(seed, network)
         let extNode = root.deriveHardened(44).deriveHardened(1).deriveHardened(0).derive(0)
         let extPub = extNode.neutered().toBase58()
         let extPrv = extNode.toBase58()
-        let account = fs.writeFileSync(`../${extPub}.json`, JSON.stringify([extPrv, extPub]))
-        let account = fs.writeFileSync(`../${extPub}.json`, JSON.stringify([extPub]))
+        // let account = fs.writeFileSync(`../${extPub}.json`, JSON.stringify([extPrv, extPub]))
+        fs.writeFileSync(`./${extPub}.json`, JSON.stringify([extPub]))
       } 
   }
 
+  module.exports = Wallet;
 // console.log(bitcoin.bip32.fromSeed(bip39.mnemonicToSeed("fun swamp jump history obvious scare struggle deputy cannon village buzz state power play expose moral million lift gravity size chalk grocery scout toss"), network))
 
-let wallet = new Wallet("law used index main pony kitchen finish hospital laugh diary obtain jewel usage decade peanut")
-wallet.createOrUpdateAccount()
+// let wallet = new Wallet("law used index main pony kitchen finish hospital laugh diary obtain jewel usage decade peanut")
+// wallet.createAccount()
