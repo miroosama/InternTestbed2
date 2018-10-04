6229198bc2362a3c51fd298f5fb0c7b4e2e7a32f
igoreskin <eskinhd@gmail.com>
2018-10-04 15:50:13 -0400

    seems to be working

--- cold/classes/ETHWallet.js
+++ cold/classes/ETHWallet.js
@@ -2,20 +2,20 @@ const Web3 = require('web3')
 const fs = require('fs')
 web3 = new Web3(new Web3.providers.HttpProvider("http://13.58.39.53:8545"))
 
-exports.ETHWallet = (()=>{
-    class extends Web3 {
-    
-        constructor(mnemonic){
+exports.ETHWallet = (() => {
+    return class extends Web3 {
+
+        constructor(mnemonic) {
             super();
             this.mnemonic = mnemonic
         }
-    
-        createAccount(){
+
+        createAccount() {
             let account = this.eth.accounts.create([this.mnemonic])
             var data = fs.writeFileSync(`./accounts.json`, JSON.stringify(account))
             console.log(account)
             return account
         }
-    
+
     }
 })();
